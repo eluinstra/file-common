ff5e358ce7937bfb8814b431c978e745ae5016d1
eluinstra <edwin.luinstra@soprasteria.nl>
2025-05-08 15:35:30 +0000

    added armor test

--- src/test/java/dev/luin/file/common/encryption/Test1.java
+++ src/test/java/dev/luin/file/common/encryption/Test1.java
@@ -33,45 +33,44 @@ import org.bouncycastle.openpgp.operator.jcajce.JcaPGPKeyPair;
 import org.bouncycastle.openpgp.operator.jcajce.JcePGPDataEncryptorBuilder;
 import org.bouncycastle.openpgp.operator.jcajce.JcePublicKeyDataDecryptorFactoryBuilder;
 import org.bouncycastle.openpgp.operator.jcajce.JcePublicKeyKeyEncryptionMethodGenerator;
+import org.bouncycastle.util.Strings;
 import org.bouncycastle.util.encoders.Base64;
 import org.bouncycastle.util.io.Streams;
 import org.junit.jupiter.api.Test;
 
 import lombok.val;
 
-class Test1
-{
+class Test1 {
 	byte[] sample = Base64.decode(
-		"mQGiBEA83v0RBADzKVLVCnpWQxX0LCsevw/3OLs0H7MOcLBQ4wMO9sYmzGYn"
-	+ "xpVj+4e4PiCP7QBayWyy4lugL6Lnw7tESvq3A4v3fefcxaCTkJrryiKn4+Cg"
-	+ "y5rIBbrSKNtCEhVi7xjtdnDjP5kFKgHYjVOeIKn4Cz/yzPG3qz75kDknldLf"
-	+ "yHxp2wCgwW1vAE5EnZU4/UmY7l8kTNkMltMEAJP4/uY4zcRwLI9Q2raPqAOJ"
-	+ "TYLd7h+3k/BxI0gIw96niQ3KmUZDlobbWBI+VHM6H99vcttKU3BgevNf8M9G"
-	+ "x/AbtW3SS4De64wNSU3189XDG8vXf0vuyW/K6Pcrb8exJWY0E1zZQ1WXT0gZ"
-	+ "W0kH3g5ro//Tusuil9q2lVLF2ovJA/0W+57bPzi318dWeNs0tTq6Njbc/GTG"
-	+ "FUAVJ8Ss5v2u6h7gyJ1DB334ExF/UdqZGldp0ugkEXaSwBa2R7d3HBgaYcoP"
-	+ "Ck1TrovZzEY8gm7JNVy7GW6mdOZuDOHTxyADEEP2JPxh6eRcZbzhGuJuYIif"
-	+ "IIeLOTI5Dc4XKeV32a+bWrQidGVzdCAoVGVzdCBrZXkpIDx0ZXN0QHViaWNh"
-	+ "bGwuY29tPohkBBMRAgAkBQJAPN79AhsDBQkB4TOABgsJCAcDAgMVAgMDFgIB"
-	+ "Ah4BAheAAAoJEJh8Njfhe8KmGDcAoJWr8xgPr75y/Cp1kKn12oCCOb8zAJ4p"
-	+ "xSvk4K6tB2jYbdeSrmoWBZLdMLACAAC5AQ0EQDzfARAEAJeUAPvUzJJbKcc5"
-	+ "5Iyb13+Gfb8xBWE3HinQzhGr1v6A1aIZbRj47UPAD/tQxwz8VAwJySx82ggN"
-	+ "LxCk4jW9YtTL3uZqfczsJngV25GoIN10f4/j2BVqZAaX3q79a3eMiql1T0oE"
-	+ "AGmD7tO1LkTvWfm3VvA0+t8/6ZeRLEiIqAOHAAQNBACD0mVMlAUgd7REYy/1"
-	+ "mL99Zlu9XU0uKyUex99sJNrcx1aj8rIiZtWaHz6CN1XptdwpDeSYEOFZ0PSu"
-	+ "qH9ByM3OfjU/ya0//xdvhwYXupn6P1Kep85efMBA9jUv/DeBOzRWMFG6sC6y"
-	+ "k8NGG7Swea7EHKeQI40G3jgO/+xANtMyTIhPBBgRAgAPBQJAPN8BAhsMBQkB"
-	+ "4TOAAAoJEJh8Njfhe8KmG7kAn00mTPGJCWqmskmzgdzeky5fWd7rAKCNCp3u"
-	+ "ZJhfg0htdgAfIy8ppm05vLACAAA=");
+			"mQGiBEA83v0RBADzKVLVCnpWQxX0LCsevw/3OLs0H7MOcLBQ4wMO9sYmzGYn"
+					+ "xpVj+4e4PiCP7QBayWyy4lugL6Lnw7tESvq3A4v3fefcxaCTkJrryiKn4+Cg"
+					+ "y5rIBbrSKNtCEhVi7xjtdnDjP5kFKgHYjVOeIKn4Cz/yzPG3qz75kDknldLf"
+					+ "yHxp2wCgwW1vAE5EnZU4/UmY7l8kTNkMltMEAJP4/uY4zcRwLI9Q2raPqAOJ"
+					+ "TYLd7h+3k/BxI0gIw96niQ3KmUZDlobbWBI+VHM6H99vcttKU3BgevNf8M9G"
+					+ "x/AbtW3SS4De64wNSU3189XDG8vXf0vuyW/K6Pcrb8exJWY0E1zZQ1WXT0gZ"
+					+ "W0kH3g5ro//Tusuil9q2lVLF2ovJA/0W+57bPzi318dWeNs0tTq6Njbc/GTG"
+					+ "FUAVJ8Ss5v2u6h7gyJ1DB334ExF/UdqZGldp0ugkEXaSwBa2R7d3HBgaYcoP"
+					+ "Ck1TrovZzEY8gm7JNVy7GW6mdOZuDOHTxyADEEP2JPxh6eRcZbzhGuJuYIif"
+					+ "IIeLOTI5Dc4XKeV32a+bWrQidGVzdCAoVGVzdCBrZXkpIDx0ZXN0QHViaWNh"
+					+ "bGwuY29tPohkBBMRAgAkBQJAPN79AhsDBQkB4TOABgsJCAcDAgMVAgMDFgIB"
+					+ "Ah4BAheAAAoJEJh8Njfhe8KmGDcAoJWr8xgPr75y/Cp1kKn12oCCOb8zAJ4p"
+					+ "xSvk4K6tB2jYbdeSrmoWBZLdMLACAAC5AQ0EQDzfARAEAJeUAPvUzJJbKcc5"
+					+ "5Iyb13+Gfb8xBWE3HinQzhGr1v6A1aIZbRj47UPAD/tQxwz8VAwJySx82ggN"
+					+ "LxCk4jW9YtTL3uZqfczsJngV25GoIN10f4/j2BVqZAaX3q79a3eMiql1T0oE"
+					+ "AGmD7tO1LkTvWfm3VvA0+t8/6ZeRLEiIqAOHAAQNBACD0mVMlAUgd7REYy/1"
+					+ "mL99Zlu9XU0uKyUex99sJNrcx1aj8rIiZtWaHz6CN1XptdwpDeSYEOFZ0PSu"
+					+ "qH9ByM3OfjU/ya0//xdvhwYXupn6P1Kep85efMBA9jUv/DeBOzRWMFG6sC6y"
+					+ "k8NGG7Swea7EHKeQI40G3jgO/+xANtMyTIhPBBgRAgAPBQJAPN8BAhsMBQkB"
+					+ "4TOAAAoJEJh8Njfhe8KmG7kAn00mTPGJCWqmskmzgdzeky5fWd7rAKCNCp3u"
+					+ "ZJhfg0htdgAfIy8ppm05vLACAAA=");
 
 	int encryptionAlgorithm = SymmetricKeyAlgorithmTags.AES_256;
 	int compressionAlgorithm = CompressionAlgorithmTags.UNCOMPRESSED;
-	boolean armorOutput = true;
+	boolean armorOutput = false;
 	boolean withIntegrityPacket = true;
 	int bufferSize = 1 << 16;
 
-	static
-	{
+	static {
 		if (Objects.isNull(Security.getProvider(BouncyCastleProvider.PROVIDER_NAME)))
 			Security.addProvider(new BouncyCastleProvider());
 	}
@@ -91,36 +90,62 @@ class Test1
 
 	@Test
 	void testArmor1() throws IOException {
-		val data = "Dit is een sample.".getBytes();
+		val msg = "Dit is een sample.".getBytes();
 		val bOut = new ByteArrayOutputStream();
 		val aOut = new ArmoredOutputStream(bOut);
-		aOut.write(data);
+		aOut.write(msg);
 		aOut.close();
 		System.out.println(bOut.toString());
 		ArmoredInputStream aIn = new ArmoredInputStream(new ByteArrayInputStream(bOut.toByteArray()));
-		val msg = aIn.readAllBytes();
+		val data = aIn.readAllBytes();
 		aIn.close();
 		Assertions.assertThat(data).isEqualTo(msg);
 	}
 
 	@Test
 	void testArmor2() throws IOException {
-		val data = "Dit is een sample.".getBytes();
-		val in = new ByteArrayInputStream(data);
+		val msg = "Dit is een sample.".getBytes();
+		val in = new ByteArrayInputStream(msg);
 		val bOut = new ByteArrayOutputStream();
 		val aOut = new ArmoredOutputStream(bOut);
 		in.transferTo(aOut);
 		aOut.close();
 		System.out.println(bOut.toString());
 		ArmoredInputStream aIn = new ArmoredInputStream(new ByteArrayInputStream(bOut.toByteArray()));
-		val msg = aIn.readAllBytes();
+		val data = aIn.readAllBytes();
 		aIn.close();
 		Assertions.assertThat(data).isEqualTo(msg);
 	}
 
 	@Test
-	void encryptDecryptMultiChunkTest() throws Exception
-	{
+	void testArmor3() throws Exception {
+		byte[] msg = Strings.toByteArray("Hello, world!");
+
+		ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+		ArmoredOutputStream aOut = new ArmoredOutputStream(bOut);
+
+		PGPLiteralDataGenerator lGen = new PGPLiteralDataGenerator();
+
+		OutputStream lOut = lGen.open(aOut, PGPLiteralData.TEXT,
+				PGPLiteralData.CONSOLE, msg.length, new Date());
+
+		lOut.write(msg);
+		lOut.close();
+
+		aOut.close();
+
+		System.out.println(Strings.fromByteArray(bOut.toByteArray()));
+
+		PGPObjectFactory oIn = new JcaPGPObjectFactory(new ArmoredInputStream(new ByteArrayInputStream(bOut.toByteArray())));
+
+		val ld = (PGPLiteralData)oIn.nextObject();
+		byte[] data = Streams.readAll(ld.getDataStream());
+
+		Assertions.assertThat(data).isEqualTo(msg);
+	}
+
+	@Test
+	void encryptDecryptMultiChunkTest() throws Exception {
 		SecureRandom random = new SecureRandom();
 		byte[] msg = new byte[60000];
 
@@ -130,14 +155,15 @@ class Test1
 
 		kpGen.initialize(2048);
 
-		PGPKeyPair pgpKp = new JcaPGPKeyPair(PGPPublicKey.RSA_GENERAL , kpGen.generateKeyPair(), new Date());
+		PGPKeyPair pgpKp = new JcaPGPKeyPair(PGPPublicKey.RSA_GENERAL, kpGen.generateKeyPair(), new Date());
 
 		PGPPublicKey pubKey = pgpKp.getPublicKey();
 
 		PGPPrivateKey privKey = pgpKp.getPrivateKey();
 
 		ByteArrayOutputStream cbOut = new ByteArrayOutputStream();
-		JcePGPDataEncryptorBuilder encryptorBuilder = new JcePGPDataEncryptorBuilder(PGPEncryptedData.AES_128).setSecureRandom(random).setProvider("BC");
+		JcePGPDataEncryptorBuilder encryptorBuilder = new JcePGPDataEncryptorBuilder(PGPEncryptedData.AES_128)
+				.setSecureRandom(random).setProvider("BC");
 
 		encryptorBuilder.setUseV5AEAD();
 		encryptorBuilder.setWithAEAD(AEADAlgorithmTags.OCB, 6);
@@ -149,7 +175,8 @@ class Test1
 		ByteArrayOutputStream ldbOut = new ByteArrayOutputStream();
 		PGPLiteralDataGenerator ldGen = new PGPLiteralDataGenerator();
 
-		OutputStream ldOut = ldGen.open(ldbOut, PGPLiteralData.BINARY, PGPLiteralData.CONSOLE, (long)msg.length, new Date());
+		OutputStream ldOut = ldGen.open(ldbOut, PGPLiteralData.BINARY, PGPLiteralData.CONSOLE, (long) msg.length,
+				new Date());
 
 		ldOut.write(msg);
 
@@ -168,18 +195,21 @@ class Test1
 
 		// decrypt
 		// Added Armor
-		PGPObjectFactory oIn = new JcaPGPObjectFactory(armorOutput ? new ArmoredInputStream(new ByteArrayInputStream(cbOut.toByteArray())) : new ByteArrayInputStream(cbOut.toByteArray()));
+		PGPObjectFactory oIn = new JcaPGPObjectFactory(
+				armorOutput ? new ArmoredInputStream(new ByteArrayInputStream(cbOut.toByteArray()))
+						: new ByteArrayInputStream(cbOut.toByteArray()));
 
-		PGPEncryptedDataList encList = (PGPEncryptedDataList)oIn.nextObject();
+		PGPEncryptedDataList encList = (PGPEncryptedDataList) oIn.nextObject();
 
-		PGPPublicKeyEncryptedData encP = (PGPPublicKeyEncryptedData)encList.get(0);
+		PGPPublicKeyEncryptedData encP = (PGPPublicKeyEncryptedData) encList.get(0);
 
-		InputStream clear = encP.getDataStream(new JcePublicKeyDataDecryptorFactoryBuilder().setProvider("BC").build(privKey));
+		InputStream clear = encP
+				.getDataStream(new JcePublicKeyDataDecryptorFactoryBuilder().setProvider("BC").build(privKey));
 
 		// System.err.println(Hex.toHexString(Streams.readAll(clear)));
 		PGPObjectFactory pgpFact = new JcaPGPObjectFactory(clear);
 
-		PGPLiteralData ld = (PGPLiteralData)pgpFact.nextObject();
+		PGPLiteralData ld = (PGPLiteralData) pgpFact.nextObject();
 
 		// isEquals("wrong filename", PGPLiteralData.CONSOLE, ld.getFileName());
 		Assertions.assertThat(ld.getFileName()).isEqualTo(PGPLiteralData.CONSOLE);
@@ -188,13 +218,12 @@ class Test1
 
 		Assertions.assertThat(data).isEqualTo(msg);
 		// isTrue("msg mismatch", Arrays.areEqual(msg, data));
-}
+	}
 
-@Test
-void encryptDecryptMultiChunkBoundaryTest() throws Exception
-{
+	@Test
+	void encryptDecryptMultiChunkBoundaryTest() throws Exception {
 		SecureRandom random = new SecureRandom();
-		byte[] msg = new byte[(1 << 6) * 5 - 17];     // take of literal data header
+		byte[] msg = new byte[(1 << 6) * 5 - 17]; // take of literal data header
 
 		random.nextBytes(msg);
 
@@ -202,14 +231,15 @@ void encryptDecryptMultiChunkBoundaryTest() throws Exception
 
 		kpGen.initialize(2048);
 
-		PGPKeyPair pgpKp = new JcaPGPKeyPair(PGPPublicKey.RSA_GENERAL , kpGen.generateKeyPair(), new Date());
+		PGPKeyPair pgpKp = new JcaPGPKeyPair(PGPPublicKey.RSA_GENERAL, kpGen.generateKeyPair(), new Date());
 
 		PGPPublicKey pubKey = pgpKp.getPublicKey();
 
 		PGPPrivateKey privKey = pgpKp.getPrivateKey();
 
 		ByteArrayOutputStream cbOut = new ByteArrayOutputStream();
-		JcePGPDataEncryptorBuilder encryptorBuilder = new JcePGPDataEncryptorBuilder(PGPEncryptedData.AES_128).setSecureRandom(random).setProvider("BC");
+		JcePGPDataEncryptorBuilder encryptorBuilder = new JcePGPDataEncryptorBuilder(PGPEncryptedData.AES_128)
+				.setSecureRandom(random).setProvider("BC");
 
 		encryptorBuilder.setUseV5AEAD();
 		encryptorBuilder.setWithAEAD(AEADAlgorithmTags.OCB, 6);
@@ -221,7 +251,8 @@ void encryptDecryptMultiChunkBoundaryTest() throws Exception
 		ByteArrayOutputStream ldbOut = new ByteArrayOutputStream();
 		PGPLiteralDataGenerator ldGen = new PGPLiteralDataGenerator();
 
-		OutputStream ldOut = ldGen.open(ldbOut, PGPLiteralData.BINARY, PGPLiteralData.CONSOLE, (long)msg.length, new Date());
+		OutputStream ldOut = ldGen.open(ldbOut, PGPLiteralData.BINARY, PGPLiteralData.CONSOLE, (long) msg.length,
+				new Date());
 
 		ldOut.write(msg);
 
@@ -240,18 +271,21 @@ void encryptDecryptMultiChunkBoundaryTest() throws Exception
 
 		// decrypt
 		// Added Armor
-		PGPObjectFactory oIn = new JcaPGPObjectFactory(armorOutput ? new ArmoredInputStream(new ByteArrayInputStream(cbOut.toByteArray())) : new ByteArrayInputStream(cbOut.toByteArray()));
+		PGPObjectFactory oIn = new JcaPGPObjectFactory(
+				armorOutput ? new ArmoredInputStream(new ByteArrayInputStream(cbOut.toByteArray()))
+						: new ByteArrayInputStream(cbOut.toByteArray()));
 
-		PGPEncryptedDataList encList = (PGPEncryptedDataList)oIn.nextObject();
+		PGPEncryptedDataList encList = (PGPEncryptedDataList) oIn.nextObject();
 
-		PGPPublicKeyEncryptedData encP = (PGPPublicKeyEncryptedData)encList.get(0);
+		PGPPublicKeyEncryptedData encP = (PGPPublicKeyEncryptedData) encList.get(0);
 
-		InputStream clear = encP.getDataStream(new JcePublicKeyDataDecryptorFactoryBuilder().setProvider("BC").build(privKey));
+		InputStream clear = encP
+				.getDataStream(new JcePublicKeyDataDecryptorFactoryBuilder().setProvider("BC").build(privKey));
 
 		// System.err.println(Hex.toHexString(Streams.readAll(clear)));
 		PGPObjectFactory pgpFact = new JcaPGPObjectFactory(clear);
 
-		PGPLiteralData ld = (PGPLiteralData)pgpFact.nextObject();
+		PGPLiteralData ld = (PGPLiteralData) pgpFact.nextObject();
 
 		// isEquals("wrong filename", PGPLiteralData.CONSOLE, ld.getFileName());
 		Assertions.assertThat(ld.getFileName()).isEqualTo(PGPLiteralData.CONSOLE);
